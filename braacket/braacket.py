e9130befdbc3c7765225053602c4d711891eabf9
DanJBarry <dbarry@stevens.edu>
2017-12-07 18:09:45 -0500

    Fix power ranking command

--- braacket/braacket.py
+++ braacket/braacket.py
@@ -43,17 +43,15 @@ class Braacket:
             table = soupObject.find_all(class_='panel-body')[1].table.tbody.find_all(class_='ellipsis')
             points = soupObject.find_all(class_='panel-body')[1].table.tbody.find_all(class_='min text-right')
             for player in range(10):
-                name = table[player].get_text()
+                name = table[player].get_text(strip='True')
                 player_url = 'https://www.braacket.com' + table[player].a.get('href')
                 character_url = 'https://www.braacket.com' + table[player].img.get('src')
                 description = ''
                 if len(table[player].span.find_all('img')) > 1:
                     for mains in range(len(table[player].span.find_all('img')) - 1):
                         description += table[player].span.find_all('img')[mains].get('title') + ', '
-                    description += table[player].span.find_all('img')[mains].get('title')
-                else:
-                    decription = table[player].span.img.get('title')
-                description += ' || ' + points[player].get_text()
+                description += table[player].span.find_all('img')[-1].get('title')
+                description += ' || ' + points[player].get_text(strip='True')
 
                 embed = discord.Embed(description=description)
                 embed.set_author(name=name, url=player_url, icon_url=character_url)
