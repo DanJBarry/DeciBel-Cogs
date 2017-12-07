105407cada05ac70375c8bc5b3fb78e87a5bf00d
DanJBarry <dbarry@stevens.edu>
2017-12-07 18:55:26 -0500

    Add final step to power ranking

--- braacket/braacket.py
+++ braacket/braacket.py
@@ -44,7 +44,7 @@ class Braacket:
             points = soupObject.find_all(class_='panel-body')[1].table.tbody.find_all(class_='min text-right')
             for player in range(10):
                 name = table[player].get_text(strip='True')
-                '''player_url = 'https://www.braacket.com' + table[player].a.get('href')
+                player_url = 'https://www.braacket.com' + table[player].a.get('href')
                 character_url = 'https://www.braacket.com' + table[player].img.get('src')
                 description = ''
                 if len(table[player].span.find_all('img')) > 1:
@@ -54,8 +54,8 @@ class Braacket:
                 description += ' || ' + points[player].get_text(strip='True')
 
                 embed = discord.Embed(description=description)
-                embed.set_author(name=name, url=player_url, icon_url=character_url)'''
-                await self.bot.say(name)
+                embed.set_author(name=name, url=player_url, icon_url=character_url)
+                await self.bot.say(embed=embed)
 
         except:
             await self.bot.say('Couldn\'t find the latest PR. Something broke.')
